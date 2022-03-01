943170f05ad37ff1df993ce52cae628098e6966d
cwfreemanj <50161832+cwfreemanj@users.noreply.github.com>
2022-02-28 19:21:56 -0500

    Add files via upload

---
+++ ScreenRes/ObjectScaler.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScaler : MonoBehaviour
+{
+    public Camera mainCamera;
+    public float currentAspect = .6f;
+
+
+    float DEVICE_SCREEN_ASPECT;
+
+    public Vector3 startingPos;
+    public void Start()
+    {
+        startingPos = gameObject.transform.position;
+        ScaleObjectWithScreenSize();
+
+
+    }
+    public void Update()
+    {
+        
+
+    }
+    public void ScaleObjectWithScreenSize()
+    {
+
+        float srcHeight = Screen.height;
+        float srcWidth = Screen.width;
+
+        DEVICE_SCREEN_ASPECT = srcWidth / srcHeight;
+
+        // Apply scaling to canvas object
+        if (DEVICE_SCREEN_ASPECT != currentAspect)
+        {
+            float fixAspect = DEVICE_SCREEN_ASPECT / currentAspect;
+
+            if (gameObject.GetComponent<RectTransform>())
+            {
+                gameObject.transform.localScale = new Vector3
+                  (gameObject.transform.localScale.x * fixAspect,
+                   gameObject.transform.localScale.y * fixAspect,
+                   1);
+
+                gameObject.transform.position = new Vector3
+                  (startingPos.x - (fixAspect * DEVICE_SCREEN_ASPECT > .5f ? 10 : 50),
+                   gameObject.transform.position.y,
+                   1);
+            }
+            else
+            {
+                // Apply scaling to game object
+                gameObject.transform.localScale = new Vector3
+                   (gameObject.transform.localScale.x * fixAspect,
+                    gameObject.transform.localScale.y * fixAspect,
+                    1);
+                gameObject.transform.position = new Vector3
+                   (gameObject.transform.position.x * (fixAspect),
+                    startingPos.y,
+                    1);
+
+            }
+          
+        }
+
+
+    }
+}
